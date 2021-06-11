4362ad17585901966b2318150d9f8ba2c4efb8df
cabildocl <cabildo@gmail.com>
2021-06-11 09:39:37 -0400

    se agregab batch_backups y backup

--- backup_device.py
+++ backup_device.py
@@ -1,43 +1,43 @@
-def cisco_ios(IP, USER, PASSWORD):
+def cisco_ios(IP, USER, PASSWORD, DIRECTORY="/tmp/"):
     from netmiko import ConnectHandler
     import datetime
     DATE = str(datetime.datetime.today().strftime('%Y-%m-%d'))
     device = { 'device_type':'cisco_ios', 'ip':IP, 'username':USER, 'password':PASSWORD,'global_delay_factor': 4}
     net_connect = ConnectHandler(**device)
     output = net_connect.send_command("show runn")
-    name_file = "/tmp/" + str(IP)+ "-" + DATE +".txt"
+    name_file = DIRECTORY + str(IP)+ "-" + DATE +".txt"
     file_config = open(name_file, 'w')
     file_config.writelines(output)
     file_config.close()
     net_connect.disconnect()
     return name_file
-def cisco_s300(IP, USER, PASSWORD):
+def cisco_s300(IP, USER, PASSWORD, DIRECTORY="/tmp/"):
     from netmiko import ConnectHandler
     import datetime
     DATE = str(datetime.datetime.today().strftime('%Y-%m-%d'))
     device = { 'device_type':'cisco_s300', 'ip':IP, 'username':USER, 'password':PASSWORD,'global_delay_factor': 4}
     net_connect = ConnectHandler(**device)
     output = net_connect.send_command("show runn")
-    name_file = "/tmp/" + str(IP)+ "-" + DATE +".txt"
+    name_file = DIRECTORY + str(IP)+ "-" + DATE +".txt"
     file_config = open(name_file, 'w')
     file_config.writelines(output)
     file_config.close()
     net_connect.disconnect()
     return name_file
-def huawei(IP, USER, PASSWORD):
+def huawei(IP, USER, PASSWORD, DIRECTORY="/tmp/"):
     from netmiko import ConnectHandler
     import datetime
     DATE = str(datetime.datetime.today().strftime('%Y-%m-%d'))
     device = { 'device_type':'huawei', 'ip':IP, 'username':USER, 'password':PASSWORD,'global_delay_factor': 5}
     net_connect = ConnectHandler(**device)
     output = net_connect.send_command("display current")
-    name_file = "/tmp/" + str(IP)+ "-" + DATE +".txt"
+    name_file = DIRECTORY + str(IP)+ "-" + DATE +".txt"
     file_config = open(name_file, 'w')
     file_config.writelines(output)
     file_config.close()
     net_connect.disconnect()
     return name_file
-def smartzone(IP,USER,PASSWORD):
+def smartzone(IP,USER,PASSWORD, DIRECTORY="/tmp/"):
     import requests
     import time
     import datetime
@@ -59,7 +59,7 @@ def smartzone(IP,USER,PASSWORD):
     connection3 = URL + "/api/public/v6_0/configuration/download"
     params = {"backupUUID": backup_id['id']}
     response3 = requests.get( connection3, data=json.dumps(payload), params=params,headers=headers ,verify=False )
-    name_file = "/tmp/" + str(IP)+ "-" + DATE +".bak"
+    name_file = DIRECTORY + str(IP)+ "-" + DATE +".bak"
     print(response3.headers.get('content-type'))
     try:
         file_config = open(name_file, 'wb')
@@ -73,7 +73,28 @@ def smartzone(IP,USER,PASSWORD):
     response4 = requests.delete( connection4, data=json.dumps(payload),headers=headers ,verify=False )
     return name_file
 
-
-
-
-
+def backup(IP,USER,PASSWORD, TYPE, DIRECTORY="/tmp/"):
+    try:
+        if "cisco_ios" == TYPE:
+            file_backup=cisco_ios(IP,USER,PASSWORD, DIRECTORY)
+        elif "cisco_s300" == TYPE:
+            file_backup=cisco_s300(IP,USER,PASSWORD, DIRECTORY)
+        elif "huawei" == TYPE:
+            file_backup=huawei(IP,USER,PASSWORD, DIRECTORY)
+        elif "huawei" == TYPE:
+            file_backup=smartzone(IP,USER,PASSWORD, DIRECTORY)
+        print(file_backup)
+    except:
+        print (f"Error TYPE: {TYPE}")
+def batch_backups(BATCH_FILE):
+    import pandas as pd
+    """
+    CSV FILE
+    ip,user,password,type,directory
+    """
+    df = pd.read_csv(BATCH_FILE)
+    for index, row in df.iterrows():
+        try:
+            backup(row['ip'],row['user'],row['password'],row['type'],row['directory'])
+        except:
+            print (f"Error IP: {row['ip']}")
